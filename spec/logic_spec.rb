ba2e32f0917d01cb0b2d1e6e5a87af37b9af55ad
Atsighi Bright <brytebee@gmail.com>
2021-04-15 14:16:21 +0100

    Add single  per test case expectation & Add negative expectations

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -10,13 +10,22 @@ describe Player do
       my_game = Player.new
       expect(my_game.game_name).to eql('Welcome to TIC TAC TOE game')
     end
+
+    it 'returns the name of game' do
+      my_game = Player.new
+      expect(my_game.game_name).not_to eql('super mario')
+    end
   end
 
   describe '#sign_validate' do
     it 'returns boolean state of players' do
       sign_validated = Player.new
-      expect(sign_validated.sign_validate('x')).to eql(true)
-      expect(sign_validated.sign_validate('o')).to eql(true)
+      expect(sign_validated.sign_validate('g')).to eql(false)
+    end
+
+    it 'returns boolean state of players' do
+      sign_validated = Player.new
+      expect(sign_validated.sign_validate('g')).to eql(false)
     end
   end
 
@@ -24,8 +33,11 @@ describe Player do
     it 'returns boolean state of player\'s move' do
       validate_move = Player.new
       expect(validate_move.move_validate(1)).to eql(true)
+    end
+
+    it 'returns boolean state of player\'s move' do
+      validate_move = Player.new
       expect(validate_move.move_validate(11)).to eql(false)
-      expect(validate_move.move_validate(9)).to equal(true)
     end
   end
 
@@ -33,9 +45,11 @@ describe Player do
     it 'returns the boolean state of board array\'s position' do
       state = Player.new
       expect(state.index_taken?(1, [1])).to eql(true)
-      expect(state.index_taken?(8, [7])).to eql(true)
-      expect(state.index_taken?(1, ['x'])).to eql(true) # can't figure this out
-      expect(state.index_taken?(8, ['o'])).to eql(true) # can't figure this out
+    end
+
+    it 'returns the boolean state of board array\'s position' do
+      state = Player.new
+      expect(state.index_taken?(1, %w[o x])).to eql(false)
     end
   end
 
@@ -43,9 +57,25 @@ describe Player do
     it 'returns boolean winning state' do
       won = Player.new
       expect(won.win(%w[o o o], 'o')).to eql(true)
+    end
+
+    it 'returns boolean winning state' do
+      won = Player.new
       expect(won.win(%w[o o o], 'x')).to eql(false)
+    end
+
+    it 'returns boolean winning state' do
+      won = Player.new
       expect(won.win(%w[o x o], 'x')).to eql(false)
+    end
+
+    it 'returns boolean winning state' do
+      won = Player.new
       expect(won.win(%w[x o o], 'o')).to eql(false)
+    end
+
+    it 'returns boolean winning state' do
+      won = Player.new
       expect(won.win(%w[x x x], 'x')).to eql(true)
     end
   end
